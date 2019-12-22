8dcb825131dba963cc60e00f661107d4297556df
javaxcode <58064721+javaxcode@users.noreply.github.com>
2019-12-22 22:42:40 +0700

    update1

--- application/views/templates/topbarblack.php
+++ application/views/templates/topbarblack.php
@@ -6,7 +6,7 @@
                 <div class="header-inner">
                     <div class="branding">
                         <h1 class="logo">
-                            <a href="index.html"><img src="images/logo@2x.png" alt="" width="25" height="26">javaXcode</a>
+                            <a href="<?= base_url() ?>"><img src="images/logo@2x.png" alt="" width="25" height="26">javaXcode</a>
                         </h1>
                     </div>
                     <nav id="nav">
