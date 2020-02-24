10aad749a0f14c6559710c61f6707e8cc64128b4
javaxcode <fannyjavaxcode@gmail.com>
2020-02-24 12:39:54 +0700

    edit telp

--- application/views/com/contact.php
+++ application/views/com/contact.php
@@ -1,81 +1,81 @@
 <div class="page-title-container style5">
-            <div class="page-title">
-                <div class="container">
-                    <h1 class="entry-title">Kontak</h1>
-                </div>
+    <div class="page-title">
+        <div class="container">
+            <h1 class="entry-title">Kontak</h1>
+        </div>
+    </div>
+    <ul class="breadcrumbs">
+        <li><a href="index.php">Home</a></li>
+        <li class="active">Kontak</li>
+    </ul>
+</div>
+
+<section id="content">
+    <div class="section">
+        <div class="container">
+            <div class="heading-box">
+                <h2 class="box-title"></h2>
             </div>
-            <ul class="breadcrumbs">
-                <li><a href="index.php">Home</a></li>
-                <li class="active">Kontak</li>
+            <ul class="contact-address style2 col-md-9">
+                <li class="office-address">
+                    <i class="fa fa-map-marker"></i>
+                    <div class="details">
+                        <h5>Office Address</h5>
+                        <p>Jl. Serua Raya No.2, Serua Bojongsari, Kota Depok, Jawa Barat 16517. </p>
+                    </div>
+                </li>
+                <li class="phone">
+                    <i class="fa fa-phone"></i>
+                    <div class="details">
+                        <h5>Phone</h5>
+                        <p>
+                            +62 852-2392-7725
+                            <br>
+                            +62 21-74774005
+                        </p>
+                    </div>
+                </li>
+                <li class="email-address">
+                    <i class="fa fa-envelope"></i>
+                    <div class="details">
+                        <h5>Email Address</h5>
+                        <p>
+                            info@javaxcode.com
+                            <br>
+                            cs@javaxcode.com
+                        </p>
+                    </div>
+                </li>
             </ul>
         </div>
+    </div>
 
-        <section id="content">
-            <div class="section">
-                <div class="container">
-                    <div class="heading-box">
-                        <h2 class="box-title"></h2>
-                    </div>
-                    <ul class="contact-address style2 col-md-9">
-                    <li class="office-address">
-                        <i class="fa fa-map-marker"></i>
-                        <div class="details">
-                            <h5>Office Address</h5>
-                            <p>Jl. Serua Raya No.2, Serua Bojongsari, Kota Depok, Jawa Barat 16517. </p>
-                        </div>
-                    </li>
-                    <li class="phone">
-                        <i class="fa fa-phone"></i>
-                        <div class="details">
-                            <h5>Phone</h5>
-                            <p>
-                                +62 822-1842-4811
-                                <br>
-                                +62 21-74774005
-                            </p>
-                        </div>
-                    </li>
-                    <li class="email-address">
-                        <i class="fa fa-envelope"></i>
-                        <div class="details">
-                            <h5>Email Address</h5>
-                            <p>
-                                info@javaxcode.com
-                                <br>
-                                cs@javaxcode.com
-                            </p>
-                        </div>
-                    </li>
-                </ul>
+    <div class="col-md-8 center-block text-center block">
+        <div class="heading-box">
+            <h2 class="box-title">Contact Us</h2>
+            <p class="desc-lg">Feel free to write us a message</p>
+        </div>
+        <form>
+            <div class="row">
+                <div class="form-group col-sms-6 col-sm-6">
+                    <input type="text" class="input-text full-width" placeholder="First name">
+                </div>
+                <div class="form-group col-sms-6 col-sm-6">
+                    <input type="text" class="input-text full-width" placeholder="Last name">
+                </div>
+                <div class="form-group col-sms-6 col-sm-6">
+                    <input type="text" class="input-text full-width" placeholder="Email address">
+                </div>
+                <div class="form-group col-sms-6 col-sm-6">
+                    <input type="text" class="input-text full-width" placeholder="Website">
                 </div>
             </div>
-            
-            <div class="col-md-8 center-block text-center block">
-                    <div class="heading-box">
-                        <h2 class="box-title">Contact Us</h2>
-                        <p class="desc-lg">Feel free to write us a message</p>
-                    </div>
-                    <form>
-                        <div class="row">
-                            <div class="form-group col-sms-6 col-sm-6">
-                                <input type="text" class="input-text full-width" placeholder="First name">
-                            </div>
-                            <div class="form-group col-sms-6 col-sm-6">
-                                <input type="text" class="input-text full-width" placeholder="Last name">
-                            </div>
-                            <div class="form-group col-sms-6 col-sm-6">
-                                <input type="text" class="input-text full-width" placeholder="Email address">
-                            </div>
-                            <div class="form-group col-sms-6 col-sm-6">
-                                <input type="text" class="input-text full-width" placeholder="Website">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <textarea rows="10" class="input-text full-width" placeholder="Send Message"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-md style1">Write Message</button>
-                        </div>
-                    </form>
-                </div>
-        </section>
+            <div class="form-group">
+                <textarea rows="10" class="input-text full-width" placeholder="Send Message"></textarea>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-md style1">Write Message</button>
+            </div>
+        </form>
+    </div>
+</section>
--- application/views/index.php
+++ application/views/index.php
@@ -1,173 +1,170 @@
+<div id="slideshow">
+    <div class="revolution-slider">
+        <ul>
+            <!-- SLIDE  -->
+            <!-- Slide1 -->
+            <li data-transition="zoomin" data-slotamount="7" data-masterspeed="1500">
+                <!-- MAIN IMAGE -->
+                <img src="<?= base_url() ?>assets/images/1.web-development.png" alt="">
+            </li>
 
-        <div id="slideshow">
-            <div class="revolution-slider">
-                <ul>
-                    <!-- SLIDE  -->
-                    <!-- Slide1 -->
-                    <li data-transition="zoomin" data-slotamount="7" data-masterspeed="1500">
-                        <!-- MAIN IMAGE -->
-                        <img src="<?= base_url() ?>assets/images/1.web-development.png" alt="">
-                    </li>
+            <!-- Slide2 -->
+            <li data-transition="zoomout" data-slotamount="7" data-masterspeed="1500">
+                <!-- MAIN IMAGE -->
+                <img src="<?= base_url() ?>assets/images/3.web-development.png" alt="">
+            </li>
 
-                    <!-- Slide2 -->
-                    <li data-transition="zoomout" data-slotamount="7" data-masterspeed="1500">
-                        <!-- MAIN IMAGE -->
-                        <img src="<?= base_url() ?>assets/images/3.web-development.png" alt="">
-                    </li>
+            <!-- Slide3 -->
+            <li data-transition="slidedown" data-slotamount="7" data-masterspeed="1500">
+                <!-- MAIN IMAGE -->
+                <img src="<?= base_url() ?>assets/images/2.web-development.png" alt="">
+            </li>
+        </ul>
+    </div>
+</div>
 
-                    <!-- Slide3 -->
-                    <li data-transition="slidedown" data-slotamount="7" data-masterspeed="1500">
-                        <!-- MAIN IMAGE -->
-                        <img src="<?= base_url() ?>assets/images/2.web-development.png" alt="">
-                    </li>
-                </ul>
+<section id="content">
+    <div class="section">
+        <div class="container">
+            <div class="heading-box">
+                <h2 class="box-title">Selamat datang di javaXcode</h2>
+                <p class="desc-lg">| Website Design | Web Development | Maintenance | Web Hosting |</p>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-3">
+                    <div class="icon-box style-boxed-3 animated box" data-animation-type="fadeInDown" data-animation-delay="0">
+                        <div class="icon-container">
+                            <i class="fa fa-rocket"></i>
+                        </div>
+                        <div class="box-content">
+                            <h4 class="box-title"><a href="#">Proses Cepat</a></h4>
+                            <p>Dikerjakan oleh tim developer yang berpengalaman dan professional.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="icon-box style-boxed-3 animated box" data-animation-type="fadeInDown" data-animation-delay="0.5">
+                        <div class="icon-container">
+                            <i class="fa fa-briefcase"></i>
+                        </div>
+                        <div class="box-content">
+                            <h4 class="box-title"><a href="#">Berorientasi Bisnis</a></h4>
+                            <p>Kami juga berfokus pada bagaimana membuat website yang lebih bernilai untuk pertumbuhan bisnis Anda.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="icon-box style-boxed-3 animated box" data-animation-type="fadeInDown" data-animation-delay="1">
+                        <div class="icon-container">
+                            <i class="fa fa-thumbs-o-up"></i>
+                        </div>
+                        <div class="box-content">
+                            <h4 class="box-title"><a href="#">Dukungan 24 Jam</a></h4>
+                            <p>Kami berkomitmen untuk memberikan kenyamanan berkomunikasi dengan Anda.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="icon-box style-boxed-3 animated box" data-animation-type="fadeInDown" data-animation-delay="1.5">
+                        <div class="icon-container">
+                            <i class="fa fa-shopping-cart"></i>
+                        </div>
+                        <div class="box-content">
+                            <h4 class="box-title"><a href="#">Harga Terbaik</a></h4>
+                            <p>Harga terbaik untuk kebutuhan bisnis Anda.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
 
-        <section id="content">
-            <div class="section">
-                <div class="container">
-                    <div class="heading-box">
-                        <h2 class="box-title">Selamat datang di javaXcode</h2>
-                        <p class="desc-lg">| Website Design | Web Development | Maintenance | Web Hosting |</p>
+    <div class="section">
+        <div class="container">
+            <div class="heading-box">
+                <h2 class="box-title">Portfolio</h2>
+                <p class="desc-lg">Beberapa perusahaan yang mempercayakan kebutuhan website dan sistem kepada Kami.</p>
+            </div>
+            <div class="soap-gallery carousel-style1 carousel box" data-front-width="250" data-front-height="366" data-slides="7" data-vAlign="top">
+                <div class="slides">
+                    <div>
+                        <a href="<?= base_url() ?>assets/images/portfolio/forexperia.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/forexperia.com.jpg" alt=""></a>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-md-3">
-                            <div class="icon-box style-boxed-3 animated box" data-animation-type="fadeInDown" data-animation-delay="0">
-                                <div class="icon-container">
-                                    <i class="fa fa-rocket"></i>
-                                </div>
-                                <div class="box-content">
-                                    <h4 class="box-title"><a href="#">Proses Cepat</a></h4>
-                                    <p>Dikerjakan oleh tim developer yang berpengalaman dan professional.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-md-3">
-                            <div class="icon-box style-boxed-3 animated box" data-animation-type="fadeInDown" data-animation-delay="0.5">
-                                <div class="icon-container">
-                                    <i class="fa fa-briefcase"></i>
-                                </div>
-                                <div class="box-content">
-                                    <h4 class="box-title"><a href="#">Berorientasi Bisnis</a></h4>
-                                    <p>Kami juga berfokus pada bagaimana membuat website yang lebih bernilai untuk pertumbuhan bisnis Anda.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-md-3">
-                            <div class="icon-box style-boxed-3 animated box" data-animation-type="fadeInDown" data-animation-delay="1">
-                                <div class="icon-container">
-                                    <i class="fa fa-thumbs-o-up"></i>
-                                </div>
-                                <div class="box-content">
-                                    <h4 class="box-title"><a href="#">Dukungan 24 Jam</a></h4>
-                                    <p>Kami berkomitmen untuk memberikan kenyamanan berkomunikasi dengan Anda.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-md-3">
-                            <div class="icon-box style-boxed-3 animated box" data-animation-type="fadeInDown" data-animation-delay="1.5">
-                                <div class="icon-container">
-                                    <i class="fa fa-shopping-cart"></i>
-                                </div>
-                                <div class="box-content">
-                                    <h4 class="box-title"><a href="#">Harga Terbaik</a></h4>
-                                    <p>Harga terbaik untuk kebutuhan bisnis Anda.</p>
-                                </div>
-                            </div>
-                        </div>
+                    <div>
+                        <a href="<?= base_url() ?>assets/images/portfolio/indotani.co.id.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/indotani.co.id.jpg" alt=""></a>
+                    </div>
+                    <div>
+                        <a href="<?= base_url() ?>assets/images/portfolio/javatechnic.co.id.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/javatechnic.co.id.jpg" alt=""></a>
+                    </div>
+                    <div>
+                        <a href="<?= base_url() ?>assets/images/portfolio/kdaforex.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/kdaforex.com.jpg" alt=""></a>
+                    </div>
+                    <div>
+                        <a href="<?= base_url() ?>assets/images/portfolio/lyricssinger.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/lyricssinger.com.jpg" alt=""></a>
+                    </div>
+                    <div>
+                        <a href="<?= base_url() ?>assets/images/portfolio/rabbaniputralogam.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/rabbaniputralogam.com.jpg" alt=""></a>
+                    </div>
+                    <div>
+                        <a href="<?= base_url() ?>assets/images/portfolio/stasiuntrader.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/stasiuntrader.com.jpg" alt=""></a>
+                    </div>
+                    <div>
+                        <a href="<?= base_url() ?>assets/images/portfolio/warungbroker.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/warungbroker.com.jpg" alt=""></a>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 
-            <div class="section">
-                <div class="container">
-                    <div class="heading-box">
-                        <h2 class="box-title">Portfolio</h2>
-                        <p class="desc-lg">Beberapa perusahaan yang mempercayakan kebutuhan website dan sistem kepada Kami.</p>
+    <div class="callout-box style3">
+        <div class="container">
+            <div class="callout-content">
+                <div class="callout-text">
+                    <h2>Are you ready to start project? Call us +62 852-2392-7725</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="container">
+            <div class="heading-box">
+                <h2 class="box-title">Apa kata mereka?</h2>
+            </div>
+            <div class="testimonials style1 owl-carousel" data-items="3" data-itemsPerDisplayWidth="[[0, 1], [480, 1], [768, 1], [992, 2], [1200, 3]]">
+                <div class="testimonial style1 box-lg">
+                    <div class="testimonial-image">
+                        <img src="<?= base_url() ?>assets/images/testimoni/agus.jpg" alt="">
                     </div>
-                    <div class="soap-gallery carousel-style1 carousel box" data-front-width="250" data-front-height="366" data-slides="7" data-vAlign="top">
-                        <div class="slides">
-                            <div>
-                                <a href="<?= base_url() ?>assets/images/portfolio/forexperia.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/forexperia.com.jpg" alt=""></a>
-                            </div>
-                            <div>
-                                <a href="<?= base_url() ?>assets/images/portfolio/indotani.co.id.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/indotani.co.id.jpg" alt=""></a>
-                            </div>
-                            <div>
-                                <a href="<?= base_url() ?>assets/images/portfolio/javatechnic.co.id.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/javatechnic.co.id.jpg" alt=""></a>
-                            </div>
-                            <div>
-                                <a href="<?= base_url() ?>assets/images/portfolio/kdaforex.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/kdaforex.com.jpg" alt=""></a>
-                            </div>
-                            <div>
-                                <a href="<?= base_url() ?>assets/images/portfolio/lyricssinger.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/lyricssinger.com.jpg" alt=""></a>
-                            </div>
-                            <div>
-                                <a href="<?= base_url() ?>assets/images/portfolio/rabbaniputralogam.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/rabbaniputralogam.com.jpg" alt=""></a>
-                            </div>
-                            <div>
-                                <a href="<?= base_url() ?>assets/images/portfolio/stasiuntrader.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/stasiuntrader.com.jpg" alt=""></a>
-                            </div>
-                            <div>
-                                <a href="<?= base_url() ?>assets/images/portfolio/warungbroker.com.jpg" class="soap-mfp-popup"><img src="<?= base_url() ?>assets/images/portfolio/warungbroker.com.jpg" alt=""></a>
-                            </div>
-                        </div>
+                    <div class="testimonial-content">
+                        Begitu bersih dan to the point, love it!
+                    </div>
+                    <div class="testimonial-author">
+                        <span class="testimonial-author-name">Agus</span> - <span class="testimonial-author-job">Pemilik Koperasi</span>
+                    </div>
+                </div>
+                <div class="testimonial style1 box-lg">
+                    <div class="testimonial-image">
+                        <img src="<?= base_url() ?>assets/images/testimoni/andri.jpg" alt="">
+                    </div>
+                    <div class="testimonial-content">
+                        Kerjanya sangat cepat dan professional
+                    </div>
+                    <div class="testimonial-author">
+                        <span class="testimonial-author-name">Andri</span> - <span class="testimonial-author-job">Pemilik Barbershop</span>
+                    </div>
+                </div>
+                <div class="testimonial style1 box-lg">
+                    <div class="testimonial-image">
+                        <img src="<?= base_url() ?>assets/images/testimoni/naji.jpg" alt="">
+                    </div>
+                    <div class="testimonial-content">
+                        Dukungan yang nyaman dan bisa diandalkan.
+                    </div>
+                    <div class="testimonial-author">
+                        <span class="testimonial-author-name">Naji</span> - <span class="testimonial-author-job">Distributor Pupuk</span>
                     </div>
                 </div>
             </div>
-
-            <div class="callout-box style3">
-                <div class="container">
-                    <div class="callout-content">
-                        <div class="callout-text">
-                            <h2>Are you ready to start project? Call us +62 822-1842-4811</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="section">
-                <div class="container">
-                    <div class="heading-box">
-                        <h2 class="box-title">Apa kata mereka?</h2>
-                    </div>
-                    <div class="testimonials style1 owl-carousel" data-items="3" data-itemsPerDisplayWidth="[[0, 1], [480, 1], [768, 1], [992, 2], [1200, 3]]">
-                        <div class="testimonial style1 box-lg">
-                            <div class="testimonial-image">
-                                <img src="<?= base_url() ?>assets/images/testimoni/agus.jpg" alt="">
-                            </div>
-                            <div class="testimonial-content">
-                                Begitu bersih dan to the point, love it!
-                            </div>
-                            <div class="testimonial-author">
-                                <span class="testimonial-author-name">Agus</span> - <span class="testimonial-author-job">Pemilik Koperasi</span>
-                            </div>
-                        </div>
-                        <div class="testimonial style1 box-lg">
-                            <div class="testimonial-image">
-                                <img src="<?= base_url() ?>assets/images/testimoni/andri.jpg" alt="">
-                            </div>
-                            <div class="testimonial-content">
-                                Kerjanya sangat cepat dan professional
-                            </div>
-                            <div class="testimonial-author">
-                                <span class="testimonial-author-name">Andri</span> - <span class="testimonial-author-job">Pemilik Barbershop</span>
-                            </div>
-                        </div>
-                        <div class="testimonial style1 box-lg">
-                            <div class="testimonial-image">
-                                <img src="<?= base_url() ?>assets/images/testimoni/naji.jpg" alt="">
-                            </div>
-                            <div class="testimonial-content">
-                                Dukungan yang nyaman dan bisa diandalkan.
-                            </div>
-                            <div class="testimonial-author">
-                                <span class="testimonial-author-name">Naji</span> - <span class="testimonial-author-job">Distributor Pupuk</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        
+        </div>
+    </div>
+</section>
